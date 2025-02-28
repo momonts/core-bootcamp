80a908ea4afbd8094b9a159d48ceaa3a6ec4f7c6
momonts <jeromemonte45@gmail.com>
2025-02-28 15:59:42 +0800

    .

--- 04-erc721/contracts/MyNFT.sol
+++ 04-erc721/contracts/MyNFT.sol
@@ -3,13 +3,11 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyNFT is ERC721URIStorage {
     uint256 tokenId;
 
-    constructor() ERC721("MyERC721Token", "MET") {}
+    constructor() ERC721("Dionela", "DNLA") {}
 
     function mint() external {
         _safeMint(msg.sender, tokenId);
